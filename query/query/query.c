0ae5de5cb2f1062d134601be3c554c0691e182b2
huizhang <huizhang@asrmicro.com>
2021-01-24 22:17:39 +0800

    a new query framework
    Change-Id: I4b55d37f6a38faad0c2a8887995bf872961167f9

--- query/query/query.c
+++ query/query/query.c
@@ -6,19 +6,53 @@ struct student{
 	int age;
 	char name[16];
 };
+int parseline(char *line,struct student *ptr)
+{
 
-int main(int argc, char *argv[]){
-    if(argc < 2){
-        printf("Usage: %s $num\n",argv[0]);
-        return 0;
-    }
-	FILE *fp = fopen("info.txt", "r");
+}
+int load_file(char *filename,struct student **out,int *num)
+{
+   	FILE *fp = fopen(filename, "r");
+    char line[1024];//
+    int count = 0;
+    struct student *list = NULL,*ptr = NULL;
 	if(fp == NULL)
 	{
 		printf("Unable to read info");
 		return -1;
 	}
-	struct student stu[6];
+    while(fgets(line,1024,fp)!=NULL){
+        count++;
+    }
+    *num = count;
+    printf("there are %d line in %s\n",count,filename);
+    fseek(fp, 0L, SEEK_SET);//rewind the fp
+    
+    ptr = list = calloc(count,sizeof(struct student));//calloc equal to malloc and memset
+
+    while(fgets(line,1024,fp)!=NULL){
+        //TODO parse the line
+        parseline(line,ptr);
+        ptr++;
+    }
+    fclose(fp); //must close fp when read finish
+    *out = list;
+    return 0;
+}
+int main(int argc, char *argv[]){
+    if(argc < 2){
+        printf("Usage: %s $num\n",argv[0]);
+        return 0;
+    }
+    int i = 0;
+    int count = 0;
+	struct student *stu = NULL;
+    load_file("info.txt",&stu,&count);
+	for (i = 0; i < count; i++){
+        printf("stu = %d %d %s\n",stu->num,stu->age,stu->name);
+        stu++;
+    }
+#if 0
 	int i;
 	for (i = 0; i < 6; i++)
 	{
@@ -29,7 +63,7 @@ int main(int argc, char *argv[]){
 			break;
 		}
 	}
-    
+#endif
 	int queryNum = atoi(argv[1]);
 	if(queryNum > 5)
 	{
